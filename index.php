9cc2c214a3aa00e96ec611d178f383b84b26fd2c
JoaoQuadros16 <joaoquadros1608@gmail.com>
2025-12-27 13:49:53 -0300

    fix: Corrige responsividade mobile - adiciona viewport meta tag e ajusta seção destaques

--- index.php
+++ index.php
@@ -57,6 +57,8 @@ function getPosicaoIcon($posicao) {
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
     <?php 
     SEO::renderMetaTags('home', [
         'title' => 'APAFUT - Associação de Pais e Amigos do Futebol | Caxias do Sul',
